462179fe5cec97683cab10fc20966b69940f8966
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-02-15 15:39:41 +0100

    #28 Created simple ui for add expense function. Used layouts for this.

--- src/com/cashflow/MainActivity.java
+++ src/com/cashflow/MainActivity.java
@@ -8,32 +8,36 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-	
-	public void addIncome(View view){
-		 Intent intent = new Intent(this, AddIncomeActivity.class);
-		    startActivity(intent);
-		    
-	}
-	public void addExpense(View view){
-		
-	}
-	public void listIncomes(View view){
-		
-	}
-	public void listExpenses(View view){
-		
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void addIncome(View view) {
+        Intent intent = new Intent(this, AddIncomeActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void addExpense(View view) {
+        Intent intent = new Intent(this, AddExpenseActivity.class);
+        startActivity(intent);
+    }
+
+    public void listIncomes(View view) {
+
+    }
+
+    public void listExpenses(View view) {
+
+    }
 
 }
