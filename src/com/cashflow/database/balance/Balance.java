245a2a5c3a8e12f46c1ac90777c03f4762a5d7a8
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-02-21 10:17:28 +0100

    #27 Class which will keep the balance added. For now it only can count the starting balance.

---
+++ src/com/cashflow/database/balance/Balance.java
@@ -0,0 +1,60 @@
+package com.cashflow.database.balance;
+
+import static com.cashflow.database.DatabaseContracts.AbstractStatement.COLUMN_NAME_AMOUNT;
+
+import java.math.BigDecimal;
+
+import android.database.Cursor;
+
+import com.cashflow.database.statement.StatementPersistentService;
+import com.cashflow.database.statement.StatementType;
+
+/**
+ * Class to keep the actual balance. Thread safe access to the balance field.
+ * @author Janos_Gyula_Meszaros
+ */
+public final class Balance {
+    private BigDecimal balance = new BigDecimal(0);
+    private StatementPersistentService service;
+
+    private Balance(StatementPersistentService service) {
+        this.service = service;
+    }
+
+    /**
+     * Static factory method which gets the {@link StatementPersistentService} to help count the balance.
+     * @param service
+     *            {@link StatementPersistentService}
+     * @return the Balance instance.
+     */
+    public static Balance getInstance(StatementPersistentService service) {
+        Balance balance = new Balance(service);
+        balance.countBalance();
+
+        return balance;
+    }
+
+    private void countBalance() {
+        double expenses = countSumOfStatement(service.getStatement(StatementType.Expense));
+        double incomes = countSumOfStatement(service.getStatement(StatementType.Income));
+
+        balance = BigDecimal.valueOf(incomes - expenses);
+    }
+
+    private double countSumOfStatement(Cursor statement) {
+        int index = statement.getColumnIndex(COLUMN_NAME_AMOUNT);
+        double amount = 0L;
+
+        while (!statement.isAfterLast()) {
+            amount += statement.getLong(index);
+            statement.moveToNext();
+        }
+
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance.doubleValue();
+    }
+
+}
