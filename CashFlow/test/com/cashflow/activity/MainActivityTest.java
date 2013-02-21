ae86cf798fef9efbea137ff96b0cebf7f6d44b8d
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-02-21 17:41:29 +0100

    First test added.

---
+++ CashFlow/test/com/cashflow/activity/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.cashflow.activity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
