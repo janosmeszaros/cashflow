449e08cf40ca0a4c420281855aabe565ce380d3d
Kornél Réfi <refikornel@gmail.com>
2013-02-21 18:28:32 +0100

    TestGuiceModule added.

--- CashFlow/test/com/cashflow/activity/MainActivityTest.java
+++ CashFlow/test/com/cashflow/activity/MainActivityTest.java
@@ -1,16 +1,40 @@
 package com.cashflow.activity;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
+    //    @Mock
+    //    ConvertFeetToMeterListener listener;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        TestGuiceModule module = new TestGuiceModule();
+        //        module.addBinding(ConvertFeetToMeterListener.class, listener);
+        TestGuiceModule.setUp(this, module);
+    }
+
+    @After
+    public void tearDown() {
+        TestGuiceModule.tearDown();
+    }
+
     @Test
-    public void firstTest() {
+    public void onCreateShouldAttachConversionListenerToConvertButton() {
+        //        MetricActivity activity = new MetricActivity();
+
+        //        activity.onCreate(null);
+        //
+        //        ShadowView button = Robolectric.shadowOf(activity.findViewById(R.id.convert_button));
         Assert.assertTrue(true);
     }
 }
