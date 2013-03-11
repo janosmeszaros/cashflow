482f9ce77cc2f290a3eaf1570d8509b7e65b68f9
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-03-11 16:24:49 +0100

    #36 Database prepared to recurring income.

--- CashFlow/src/com/cashflow/database/DatabaseContracts.java
+++ CashFlow/src/com/cashflow/database/DatabaseContracts.java
@@ -10,11 +10,15 @@ public abstract class DatabaseContracts {
     public static final String DATABASE_NAME = "CashFlow.db";
 
     // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     private static final String TEXT_TYPE = " TEXT";
     private static final String INTEGER_TYPE = " INTEGER";
     private static final String REAL_TYPE = " REAL";
     private static final String COMMA_SEP = ",";
+    private static final String CREATE_TABLE = "CREATE TABLE ";
+    private static final String OPENNING_BRACKET = " (";
+    private static final String CLOSSING_BRACKET = " )";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS ";
 
     /**
      * Statement table.
@@ -27,18 +31,17 @@ public abstract class DatabaseContracts {
         public static final String COLUMN_NAME_IS_INCOME = "is_income";
         public static final String COLUMN_NAME_DATE = "date";
         public static final String COLUMN_NAME_NOTE = "note";
+        public static final String COLUMN_NAME_INTERVAL = "interval";
         public static final String[] PROJECTION = new String[]{_ID, COLUMN_NAME_AMOUNT, COLUMN_NAME_DATE, COLUMN_NAME_NOTE};
 
-        public static final String EXPENSE_SELECTION = "(" + COLUMN_NAME_IS_INCOME + " == 0)";
-        public static final String INCOME_SELECTION = "(" + COLUMN_NAME_IS_INCOME + " == 1)";
+        public static final String EXPENSE_SELECTION = OPENNING_BRACKET + COLUMN_NAME_IS_INCOME + " == 0)";
+        public static final String INCOME_SELECTION = OPENNING_BRACKET + COLUMN_NAME_IS_INCOME + " == 1)";
 
-        static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DatabaseContracts.AbstractStatement.TABLE_NAME + " ("
-                + DatabaseContracts.AbstractStatement._ID + " INTEGER PRIMARY KEY," + DatabaseContracts.AbstractStatement.COLUMN_NAME_AMOUNT
-                + REAL_TYPE + COMMA_SEP + DatabaseContracts.AbstractStatement.COLUMN_NAME_IS_INCOME + INTEGER_TYPE + COMMA_SEP
-                + DatabaseContracts.AbstractStatement.COLUMN_NAME_DATE + TEXT_TYPE + COMMA_SEP + DatabaseContracts.AbstractStatement.COLUMN_NAME_NOTE
-                + TEXT_TYPE + " )";
+        static final String SQL_CREATE_ENTRIES = CREATE_TABLE + TABLE_NAME + OPENNING_BRACKET + _ID + " INTEGER PRIMARY KEY" + COMMA_SEP
+                + COLUMN_NAME_AMOUNT + REAL_TYPE + COMMA_SEP + COLUMN_NAME_IS_INCOME + INTEGER_TYPE + COMMA_SEP + COLUMN_NAME_DATE + TEXT_TYPE
+                + COMMA_SEP + COLUMN_NAME_NOTE + TEXT_TYPE + COMMA_SEP + COLUMN_NAME_INTERVAL + TEXT_TYPE + CLOSSING_BRACKET;
 
-        static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DatabaseContracts.AbstractStatement.TABLE_NAME;
+        static final String SQL_DELETE_ENTRIES = DROP_TABLE + TABLE_NAME;
 
         private AbstractStatement() {
         }
@@ -54,11 +57,10 @@ public abstract class DatabaseContracts {
         public static final String COLUMN_NAME_CATEGORY_NAME = "name";
         public static final String[] PROJECTION = new String[]{_ID, COLUMN_NAME_CATEGORY_NAME};
 
-        static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DatabaseContracts.AbstractCategory.TABLE_NAME + " ("
-                + DatabaseContracts.AbstractCategory._ID + " INTEGER PRIMARY KEY," + DatabaseContracts.AbstractCategory.COLUMN_NAME_CATEGORY_NAME
-                + REAL_TYPE + " )";
+        static final String SQL_CREATE_ENTRIES = CREATE_TABLE + TABLE_NAME + OPENNING_BRACKET + _ID + " INTEGER PRIMARY KEY,"
+                + COLUMN_NAME_CATEGORY_NAME + REAL_TYPE + CLOSSING_BRACKET;
 
-        static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DatabaseContracts.AbstractCategory.TABLE_NAME;
+        static final String SQL_DELETE_ENTRIES = DROP_TABLE + TABLE_NAME;
 
         private AbstractCategory() {
         }
