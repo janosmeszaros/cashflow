8e7c80e970c82b4f9918764f07e416140d148258
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-03-21 14:19:20 +0100

    #48 Simple UI created for add bill.

---
+++ CashFlow/src/com/cashflow/activity/AddBillActivity.java
@@ -0,0 +1,91 @@
+package com.cashflow.activity;
+
+import static android.view.View.VISIBLE;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import roboguice.activity.RoboFragmentActivity;
+import roboguice.inject.InjectView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import com.cashflow.R;
+import com.cashflow.activity.listeners.DateButtonOnClickListener;
+import com.cashflow.activity.listeners.RecurringCheckBoxOnClickListener;
+import com.cashflow.database.category.CategoryPersistenceService;
+import com.cashflow.domain.Category;
+import com.google.inject.Inject;
+
+/**
+ * Add bills.
+ * @author Janos_Gyula_Meszaros
+ *
+ */
+public class AddBillActivity extends RoboFragmentActivity {
+    @InjectView(R.id.categorySpinner)
+    private Spinner categorySpinner;
+    @InjectView(R.id.dateButton)
+    private Button dateButton;
+    @InjectView(R.id.recurring_area)
+    private LinearLayout recurringArea;
+    @InjectView(R.id.recurring_spinner)
+    private Spinner recurringSpinner;
+    @InjectView(R.id.recurring_checkbox)
+    private CheckBox recurringCheckBox;
+
+    @Inject
+    private DateButtonOnClickListener listener;
+    @Inject
+    private RecurringCheckBoxOnClickListener checkBoxListener;
+    @Inject
+    private SpinnerAdapter spinnerAdapter;
+    @Inject
+    private CategoryPersistenceService categoryService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bill);
+
+        setUpDateButton();
+        activateRecurringArea();
+        setCategorySpinner();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_bill, menu);
+        return true;
+    }
+
+    private void setCategorySpinner() {
+        List<Category> list = categoryService.getCategories();
+
+        ArrayAdapter<Category> adapter = new ArrayAdapter<Category>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        categorySpinner.setAdapter(adapter);
+    }
+
+    private void setUpDateButton() {
+        final Calendar calendar = Calendar.getInstance();
+        DateFormat fmtDateAndTime = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        dateButton.setText(fmtDateAndTime.format(calendar.getTime()));
+
+        dateButton.setOnClickListener(listener);
+    }
+
+    private void activateRecurringArea() {
+        recurringCheckBox.setOnClickListener(checkBoxListener);
+        recurringArea.setVisibility(VISIBLE);
+        recurringSpinner.setAdapter(spinnerAdapter);
+    }
+
+}
