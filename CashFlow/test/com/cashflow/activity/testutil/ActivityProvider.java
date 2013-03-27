e967ccb7d5d4787690425a7fcb6e9f23bc7c5e85
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-03-27 10:23:45 +0100

    Fixed the previous commit.

--- CashFlow/test/com/cashflow/activity/testutil/ActivityProvider.java
+++ CashFlow/test/com/cashflow/activity/testutil/ActivityProvider.java
@@ -1,18 +1,19 @@
 package com.cashflow.activity.testutil;
 
-import com.cashflow.bill.activity.AddBillActivity;
+import android.app.Activity;
+
 import com.google.inject.Provider;
 
 /**
- * Provider for {@link AddBillActivity} class.
+ * Provider for {@link Activity} class.
  * @author Janos_Gyula_Meszaros
  *
  */
-public class AddBillActivityProvider implements Provider<AddBillActivity> {
+public class ActivityProvider implements Provider<Activity> {
 
     @Override
-    public AddBillActivity get() {
-        return new AddBillActivity();
+    public Activity get() {
+        return new Activity();
     }
 
 }
