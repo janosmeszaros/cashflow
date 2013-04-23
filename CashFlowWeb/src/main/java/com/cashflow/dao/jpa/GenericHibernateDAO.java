35ca34bc4c1ecf99561cafcd0d01c2f5fc50ed10
Janos Gyula Meszaros <janos_gyula_meszaros@epam.com>
2013-04-23 14:01:53 +0200

    Generic hibernate dao added to avoid duplication.

---
+++ CashFlowWeb/src/main/java/com/cashflow/dao/jpa/GenericHibernateDAO.java
@@ -0,0 +1,108 @@
+package com.cashflow.dao.jpa;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Implements basic dao features with hibernate.
+ * @author Janos_Gyula_Meszaros
+ * @param <T>
+ */
+public class GenericHibernateDAO<T> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericHibernateDAO.class);
+
+    private final SessionFactory sessionFactory;
+
+    private final Class<T> persistentClass;
+
+    /**
+     * Constructor.
+     * @param sessionFactory
+     *            sessionFactory
+     * @param persistentClass
+     *            class to persist
+     */
+    @Autowired
+    public GenericHibernateDAO(final Class<T> persistentClass, final SessionFactory sessionFactory) {
+        super();
+        this.persistentClass = persistentClass;
+        this.sessionFactory = sessionFactory;
+    }
+
+    /**
+     * Persist the given entity to db.
+     * @param entity
+     *            entity.
+     * @return <code>true</code> if succesfull.
+     */
+    @Transactional
+    public boolean persist(final T entity) {
+        boolean isSuccess = false;
+        try {
+            sessionFactory.getCurrentSession().persist(entity);
+            isSuccess = true;
+        } catch (final HibernateException e) {
+            LOGGER.error("An exception occured during the operations. Exception message: " + e.getMessage());
+        }
+        return isSuccess;
+    }
+
+    /**
+     * Merge the given entity to db.
+     * @param entity
+     *            entity
+     * @return <code>true</code> if successful.
+     */
+    @Transactional
+    public boolean merge(final T entity) {
+        boolean isSuccess = false;
+        try {
+            sessionFactory.getCurrentSession().merge(entity);
+            isSuccess = true;
+        } catch (final HibernateException e) {
+            LOGGER.error("An exception occured during the operations. Exception message: " + e.getMessage());
+        }
+        return isSuccess;
+    }
+
+    /**
+     * Get entities by one or more criteria.
+     * @param criterion
+     *            set of {@link Criterion}
+     * @return list of entities.
+     */
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<T> findByCriteria(final Criterion... criterion) {
+        final Criteria crit = sessionFactory.getCurrentSession().createCriteria(persistentClass);
+        if (criterion != null) {
+            for (final Criterion c : criterion) {
+                crit.add(c);
+            }
+        }
+
+        return crit.list();
+    }
+
+    /**
+     * Find an entity by id.
+     * @param entityId
+     *            id
+     * @return entity.
+     */
+    @SuppressWarnings("unchecked")
+    @Transactional
+    public T findById(final long entityId) {
+        return (T) sessionFactory.getCurrentSession().load(persistentClass, entityId);
+
+    }
+
+}
--- CashFlowWeb/src/main/java/com/cashflow/dao/jpa/JPABasedStatementDAO.java
+++ CashFlowWeb/src/main/java/com/cashflow/dao/jpa/JPABasedStatementDAO.java
@@ -1,19 +1,18 @@
 package com.cashflow.dao.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.Validate;
 import org.dozer.Mapper;
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.cashflow.dao.StatementDAO;
 import com.cashflow.dao.objects.StatementEntity;
+import com.cashflow.domain.RecurringInterval;
 import com.cashflow.domain.Statement;
 import com.cashflow.domain.StatementType;
 
@@ -23,44 +22,29 @@ import com.cashflow.domain.StatementType;
  */
 @Component
 public class JPABasedStatementDAO implements StatementDAO {
-    private static final String TYPE = "type";
-    private static final String GET_STATEMENTS_BY_TYPE = "Statement.getStatementsByType";
-    private static final Logger LOGGER = LoggerFactory.getLogger(JPABasedStatementDAO.class);
-    private final SessionFactory sessionFactory;
     private final Mapper mapper;
+    private final GenericHibernateDAO<StatementEntity> dao;
 
     /**
      * Constructor.
-     * @param sessionFactory
-     *            sessionFactory.
      * @param mapper
      *            Dozer mapper.
+     * @param dao
+     *            dao.
      */
     @Autowired
-    public JPABasedStatementDAO(final SessionFactory sessionFactory, final Mapper mapper) {
+    public JPABasedStatementDAO(final Mapper mapper, final GenericHibernateDAO<StatementEntity> dao) {
         super();
-        this.sessionFactory = sessionFactory;
         this.mapper = mapper;
+        this.dao = dao;
     }
 
     @Override
-    @Transactional
     public boolean save(final Statement statement) {
         Validate.notNull(statement);
 
         final StatementEntity entity = generateEntity(statement);
-        return persistStatementEntity(entity);
-    }
-
-    private boolean persistStatementEntity(final StatementEntity entity) {
-        boolean isSuccess = false;
-        try {
-            sessionFactory.getCurrentSession().persist(entity);
-            isSuccess = true;
-        } catch (final HibernateException e) {
-            LOGGER.error("An exception occured during the operations. Exception message: " + e.getMessage());
-        }
-        return isSuccess;
+        return dao.persist(entity);
     }
 
     private StatementEntity generateEntity(final Statement statement) {
@@ -68,61 +52,59 @@ public class JPABasedStatementDAO implements StatementDAO {
     }
 
     @Override
-    @Transactional
     public boolean update(final Statement statement, final String statementId) {
         Validate.notNull(statement);
         Validate.notEmpty(statementId);
 
         final StatementEntity entity = generateEntity(statement);
-        return mergeStatement(entity);
+        return dao.merge(entity);
     }
 
-    private boolean mergeStatement(final StatementEntity entity) {
-        boolean isSuccess = false;
-        try {
-            sessionFactory.getCurrentSession().merge(entity);
-            isSuccess = true;
-        } catch (final HibernateException e) {
-            LOGGER.error("An exception occured during the operations. Exception message: " + e.getMessage());
-        }
-        return isSuccess;
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
-    @Transactional
     public List<Statement> getAllStatements() {
-        return sessionFactory.getCurrentSession().getNamedQuery("Statement.getAllStatements").list();
+        final List<StatementEntity> entityList = dao.findByCriteria();
+        return convertToStatementList(entityList);
     }
 
-    @SuppressWarnings("unchecked")
+    private List<Statement> convertToStatementList(final List<StatementEntity> entityList) {
+        final List<Statement> statementList = new ArrayList<Statement>();
+
+        for (final StatementEntity entity : entityList) {
+            final Statement statement = convertToStatement(entity);
+            statementList.add(statement);
+        }
+
+        return statementList;
+    }
+
+    private Statement convertToStatement(final StatementEntity entity) {
+        return mapper.map(entity, Statement.class);
+    }
+
     @Override
-    @Transactional
     public List<Statement> getExpenses() {
-        return sessionFactory.getCurrentSession().getNamedQuery(GET_STATEMENTS_BY_TYPE)
-                .setString(TYPE, StatementType.Expense.toString()).list();
+        final SimpleExpression expression = Restrictions.eq("type", StatementType.Expense);
+        final List<StatementEntity> entityList = dao.findByCriteria(expression);
+        return convertToStatementList(entityList);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    @Transactional
     public List<Statement> getIncomes() {
-        return sessionFactory.getCurrentSession().getNamedQuery(GET_STATEMENTS_BY_TYPE)
-                .setString(TYPE, StatementType.Income.toString()).list();
+        final SimpleExpression expression = Restrictions.eq("type", StatementType.Income);
+        final List<StatementEntity> entityList = dao.findByCriteria(expression);
+        return convertToStatementList(entityList);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    @Transactional
     public List<Statement> getRecurringIncomes() {
-        return sessionFactory.getCurrentSession().getNamedQuery("Statement.getRecurringStatements").list();
+        final SimpleExpression expression = Restrictions.ne("recurringInterval", RecurringInterval.none);
+        final List<StatementEntity> entityList = dao.findByCriteria(expression);
+        return convertToStatementList(entityList);
     }
 
     @Override
-    @Transactional
     public Statement getStatementById(final String statementId) {
-        return (Statement) sessionFactory.getCurrentSession().getNamedQuery("Statement.getStatementsById")
-                .setInteger("id", Integer.parseInt(statementId)).uniqueResult();
+        final StatementEntity entity = dao.findById(Long.parseLong(statementId));
+        return convertToStatement(entity);
     }
-
 }
